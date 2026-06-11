bf814e9f78a0d0edb76b6c4740c282f1fd8fb54c
Saheli Mete <sahelimete97@gmail.com>
2026-06-11 16:04:38 +0530

    Complete chapter-1

---
+++ Chapter-1/Module.py
@@ -0,0 +1,15 @@
+import pyjokes
+
+# print("Saheli is a good girl")
+
+joke = pyjokes.get_joke()
+print(joke)
+
+# Single line comment
+
+"""
+This is a multi line comment
+
+"""
+'''
+This is also a multi line comment'''
